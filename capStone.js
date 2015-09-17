0c082b4fd3f68cd1255ad30625b73d9bb0bebe65
johnhur <johnjhur@gmail.com>
2015-09-16 21:20:37 -0700

    updating models..

--- capStone.js
+++ capStone.js
@@ -1,6 +1,7 @@
 
-Choice1 = new Mongo.Collection('choice1')
-Choice2 = new Mongo.Collection('choice2')
+// Choice1 = new Mongo.Collection('choice1')
+// Choice2 = new Mongo.Collection('choice2')
+Competition = new Mongo.Collection('competition') 
 
 // having a new instance collection, having name and hashes as parameters.. 
 
@@ -14,24 +15,48 @@ if (Meteor.isClient) {
 
   angular.module('tweet-vote').controller('CapStoneCtrl', ['$scope', '$meteor',
     function($scope, $meteor) {
-   
-      $scope.tweets1 = $meteor.collection(Choice1)
-      $scope.tweets2 = $meteor.collection(Choice2)
+      // $scope.tweets1 = $meteor.collection(Choice1)
+      // $scope.tweets2 = $meteor.collection(Choice2)
+      $scope.competition = $meteor.collection(Competition)
+
+      // remodeling the data..
+      var data3 = $scope.getCollectionReactively('competition')
+
+      $scope.createPoll = function(){
+       var comp = Competition.insert({
+          name: $scope.competitionName,
+          key1: {
+            word: $scope.userInput1, 
+            tweets: [] 
+          },
+          key2: {
+            word: $scope.userInput2,
+            tweets: []
+          }
+        })
+       console.log(comp)
+      }
 
      $meteor.autorun($scope, function(){
-       var data1 = $scope.getCollectionReactively('tweets1')
-       var data2 = $scope.getCollectionReactively('tweets2')
-      newScore1 = data1.length
-      newScore2 = data2.length  
+       // var data1 = $scope.getCollectionReactively('tweets1')
+       // var data2 = $scope.getCollectionReactively('tweets2')
 
-      $scope.labels = [$scope.userInput1, $scope.userInput2];
-      $scope.data = [newScore1, newScore2]  
+        // owner key on competition
+        // url = competition/:id
+        
+        // newScore1 = data.key1.tweets.length
+        // newScore2 = data.key2.tweets.length
+
+      // } else {
+      // newScore1 = data.key1.tweets.length
+      // newScore2 = data.key2.tweets.length
+
+      // $scope.labels = [$scope.userInput1, $scope.userInput2];
+      // $scope.data = [newScore1, newScore2]  
+    
+
+    })
        //this runs everytime collection updates
-      });
-
-       
-
-      
       // $meteor.collection(Tweets).forEach(function(t){
       //   if ("#" + t.choice == $scope.userInput1){
       //     newScore1 += 1
@@ -45,22 +70,11 @@ if (Meteor.isClient) {
       //     console.log("NOOOO")
       //   }
       // })
-      
-      
-                    
 
 
   $scope.getTweets = function (word1, word2){
-  	$meteor.call('getTweets', word1, word2, function(err, response){
-  		if (err){
-        console.log("RESPONSE INVALID")
-  			console.log(err)
-  		} else {
-      console.log("RESPONSE VALID")
-  		console.log(response);
-  	}
-  	});
-	  }
+  	$meteor.call('getTweets', word1, word2);
+	}
 
   $scope.stopStream = function(){
     $meteor.call('stopStream')
@@ -82,7 +96,7 @@ Meteor.methods({
         })
 
         stream.on('limit', function (limitMessage) {
-          console.log("limit message: " + limitMessage)
+          console.log("limit message: " + limitMessage.message)
         })
 
         stream.on('error', function (error) {
@@ -98,26 +112,75 @@ Meteor.methods({
               user.name = tweet["user"]["name"]
               user.text = tweet["text"]
 
-              Choice1.insert({
-                name: user.name,
-                text: user.text,
-                choice: user.choice,
-                owner: Meteor.userId()
-              });
+                // Competition.insert({
+                //   choice1: {
+                //     word: "CAT", 
+                //     tweets: ["savannah's are cute", "I love blue cats"] 
+                //   },
+                //   choice2: {
+                //     word: "dog",
+                //     tweets: ["frenchy's rule", "dogs are smelly"]
+                //   }
+                // })
+
+              // Choice1.insert({
+              //   name: user.name,
+              //   text: user.text,
+              //   choice: user.choice,
+              //   owner: Meteor.userId()
+              // });
             }
             else if("#" + hash.text == word2){
               user.choice = hash.text
               user.name = tweet["user"]["name"]
               user.text = tweet["text"]
 
-              Choice2.insert({
-                name: user.name,
-                text: user.text,
-                choice: user.choice,
-                owner: Meteor.userId()
-              });
-            }
-          })
+                // Competition.insert({
+                //   key1: {
+                //     word: "CAT", 
+                //     tweets: ["savannah's are cute", "I love blue cats"] 
+                //   },
+                //   key2: {
+                //     word: "dog",
+                //     tweets: ["frenchy's rule", "dogs are smelly"]
+                //   }
+                // })
+              }
+
+              })
+
+              // Choice2.insert({
+              //   name: user.name,
+              //   text: user.text,
+              //   choice: user.choice,
+              //   owner: Meteor.userId()
+              // });
+
+
+              }))
+
+              // Competition.insert({
+              //   key1: {
+              //     word: "CAT", 
+              //     tweets: ["savannah's are cute", "I love blue cats"] 
+              //   },
+              //   key2: {
+              //     word: "dog",
+              //     tweets: ["frenchy's rule", "dogs are smelly"]
+              //   }
+              // })
+          
+          },
+          //     Competition.insert({
+          //       key1.tweets.push(hash.text)
+          //     });
+          //   }
+
+                     //  Tweets.insert({
+          //   competition_name: exampledId,
+          //   word: "cat",
+          //   phrase: "cats rule"
+          // })
           
           // once I input new search parameters on client, 
           // the user.choice becomes null for all tweets in database. 
@@ -131,8 +194,7 @@ Meteor.methods({
             // });
             // user = {}
             // }
-        }));
-  	},
+  
     stopStream: function() {
       stream.stop()
     }
